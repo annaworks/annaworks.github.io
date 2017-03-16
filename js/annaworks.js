4b188e2897489295210588f88c17576f4ee0e3ff
annaworks <cha.yuki@gmail.com>
2017-03-16 08:02:50 -0700

    card-modals layout complete

--- js/annaworks.js
+++ js/annaworks.js
@@ -11,11 +11,37 @@ $(window).on('scroll', function() {
     $('#nav-fix').css('display', 'none');
     $('#nav-fix').removeClass('fixed');
     $('#nav-fix').removeClass('opac-95');
-  }
+  };
+
 });
 
+// AFTER PAGE LOAD
 $(function() {
+  // Header Fade in Animation
   $('#silhouette').hide().delay(400).fadeIn(1000);
   $('#workTitle').hide().delay(800).fadeIn(800);
+
+  // Card Layover Show on click
+  $('.card').on('click', function(){
+    var a=$(this).attr('id');
+    $('#modal-'+a).fadeIn();
+    $('#modal-img-'+a).fadeIn(50);
+    $('.modal-overlay').css('display', 'block');
+    $(window).on('click', function(e){
+      var b=e.toElement.className;
+      var c=e.toElement.innerText;
+      if(b=='modal-overlay' || c=='CLOSE') {
+        $('.modal-overlay').hide();
+        $('#modal-'+a).fadeOut();
+        $('#modal-img-'+a).fadeOut(50);
+      }
+    });
+  });
+
+
+
+  // if($('.modal').is(":visible")==true) {
+  //
+  // }
 
 });
